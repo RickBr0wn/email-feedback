6694add56b2496acf3d626d85c5a07b79acef755
RiCK BROWN <ricky.brown.00@gmail.com>
2018-06-01 00:09:01 +0100

    fixed untracked files

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-// Development keys
-module.exports = {
-  googleClientID: '476607806540-tstehlgjkpk5t3b4ihjtf0p7i89vi7ap.apps.googleusercontent.com',
-  googleClientSecret: '72XAwf1TPX7C3hrkQ5-Y7oU7',
-  mongoURI: 'mongodb://RiCK_BROWN:Harry123@ds141870.mlab.com:41870/email-e',
-  cookieKey: 'Rick loves development'
-}
