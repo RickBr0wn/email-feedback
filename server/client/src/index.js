fb8fde0157522fb75266aee66bc49913b1fd3a4e
RiCK BROWN <ricky.brown.00@gmail.com>
2018-06-01 16:47:50 +0100

    Added working BrowerRouter & Routes

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,7 +1,18 @@
+// Libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Redux
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
 // Component
 import App from './Components/App'
+import reducers from './Reducers'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+const store = createStore(() => [], {}, applyMiddleware())
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.querySelector('#root'))
